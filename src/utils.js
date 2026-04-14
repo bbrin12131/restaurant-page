04f97666a519dd8b84af0b0c284a65af320329dc
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-04-14 21:11:33 +0500

    Refactor

--- src/utils.js
+++ src/utils.js
@@ -11,37 +11,43 @@ function showHeading(text, level) {
 }
 
 function showParagraph(text) {
-  contentElement.appendChild(createParagraph(text, "content__paragraph"));
+  const element = createParagraph(text, "content__paragraph");
+  contentElement.appendChild(element);
 }
 
 function showImage(image, alt, width = 0, height = 0) {
-  contentElement.appendChild(
-    createImage(image, alt, "content__image", width, height),
-  );
+  const element = createImage(image, alt, "content__image", width, height);
+  contentElement.appendChild(element);
 }
 
 function showCard(name, image, alt, imageWidth = 0, imageHeight = 0) {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  card.appendChild(createParagraph(name, "card__name"));
+  const nameElement = createParagraph(name, "card__name");
+  card.appendChild(nameElement);
 
-  card.appendChild(
-    createImage(image, alt, "card__image", imageWidth, imageHeight),
+  const imageElement = createImage(
+    image,
+    alt,
+    "card__image",
+    imageWidth,
+    imageHeight,
   );
+  card.appendChild(imageElement);
 
   contentElement.appendChild(card);
 }
 
 function createParagraph(text, className) {
   const element = document.createElement("p");
-  return fillTextElement(element, text, className);
+  fillTextElement(element, text, className);
+  return element;
 }
 
 function fillTextElement(element, text, className) {
   element.classList.add(className);
   element.textContent = text;
-  return element;
 }
 
 function createImage(image, alt, className, width = 0, height = 0) {
