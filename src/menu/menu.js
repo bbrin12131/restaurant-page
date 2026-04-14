4ec06ef64a11badc6e70ae7d416ec3c102a02f6a
Bogdan V. Brinzey <bbrinzey@gmail.com>
2026-04-14 21:31:25 +0500

    Refactor

--- src/menu/menu.js
+++ src/menu/menu.js
@@ -7,27 +7,27 @@ const DISH_CARD_IMAGE_WIDTH = 500;
 const DISH_CARD_IMAGE_HEIGHT = 300;
 
 export default function () {
+  const entries = [
+    [
+      "Vanilla Ice Cream",
+      vanillaIceCreamImage,
+      "Two scoops of vanilla ice cream in a bowl, with a spoon on a cloth nearby",
+    ],
+    [
+      "Chocolate Ice Cream",
+      chocolateIceCreamImage,
+      "Three scoops of chocolate ice cream in a white bowl on a yellow background",
+    ],
+    [
+      "Strawberry Ice Cream",
+      strawberryIceCreamImage,
+      "Three scoops of strawberry ice cream with a spoon in a bowl",
+    ],
+  ];
+
   resetContent();
   showHeading("Menu", 1);
-  showCard(
-    "Vanilla Ice Cream",
-    vanillaIceCreamImage,
-    "Two scoops of vanilla ice cream in a bowl, with a spoon on a cloth nearby",
-    DISH_CARD_IMAGE_WIDTH,
-    DISH_CARD_IMAGE_HEIGHT,
-  );
-  showCard(
-    "Chocolate Ice Cream",
-    chocolateIceCreamImage,
-    "Three scoops of chocolate ice cream in a white bowl on a yellow background",
-    DISH_CARD_IMAGE_WIDTH,
-    DISH_CARD_IMAGE_HEIGHT,
-  );
-  showCard(
-    "Strawberry Ice Cream",
-    strawberryIceCreamImage,
-    "Three scoops of strawberry ice cream with a spoon in a bowl",
-    DISH_CARD_IMAGE_WIDTH,
-    DISH_CARD_IMAGE_HEIGHT,
-  );
+  for (const entry of entries) {
+    showCard(...entry, DISH_CARD_IMAGE_WIDTH, DISH_CARD_IMAGE_HEIGHT);
+  }
 }
